f3fb9ca44f81eab8943d81b6495fc6d2b2df971a
gregmarquet <marquet.greg@gmail.com>
2017-03-25 08:14:38 -0400

    set lastX and lastY to e.offsetX and e.offsetY in draw function and mousedown eventListener and change ctx.lineWidth

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 10;
 
 let isDrawing = false;
 let lastX = 0;
@@ -15,10 +16,22 @@ let lastY = 0;
 function draw(e) {
   if(!isDrawing) return; // stop the function from running when not moused (mousedown)
   console.log(e);
+  ctx.beginPath();
+  // start from
+  ctx.moveTo(lastX, lastY);
+  // go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+
 }
 
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
