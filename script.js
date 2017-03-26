8619da9f7242421ea814da15d8a4f01137c29145
gregmarquet <marquet.greg@gmail.com>
2017-03-26 17:08:24 -0400

    change canvas size and add info div with color divs

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
