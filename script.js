33c6f5ca6ffcc9cb3ce5b2ad9f3ba2f9b88c1af0
gregmarquet <marquet.greg@gmail.com>
2017-03-27 09:30:41 -0400

    add const colors and eventListener on each color element to change strokeStyle

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
+// const red = document.getElementById('red');
+// const blue = document.getElementById('blue');
+const colors = document.querySelectorAll('.color');
 
 // canvas.width = window.innerWidth;
 // canvas.height = window.innerHeight;
 
-ctx.strokeStyle = '#BADA55';
+let positionInfo = canvas.getBoundingClientRect();
+canvas.height = positionInfo.height;
+canvas.width = positionInfo.width;
+
+
+ctx.strokeStyle = 'black';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 10;
@@ -36,6 +44,20 @@ canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
 
+
+
+// canvas.addEventListener('touchstart', (e) => {
+//   isDrawing = true;
+//   [lastX, lastY] = [e.offsetX, e.offsetY];
+// });
+
+// canvas.addEventListener('touchmove', draw);
+// canvas.addEventListener('touchend', () => isDrawing = false);
+// canvas.addEventListener('touchleave', () => isDrawing = false);
+
+colors.forEach((color) => {
+  color.addEventListener('click', () => ctx.strokeStyle = color.dataset.color);
+});
 
 
 
